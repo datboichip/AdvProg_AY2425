98c535d73b2c1739a63f69b6ed2f962e52555022
datboichip <vietphuongchip2006@gmail.com>
2025-03-12 14:13:19 +0700

    Update guessit.cpp
    update guessit

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -14,7 +14,8 @@ using namespace std;
 ***/
 int generateRandomNumber() {
     // TODO: Return the random number in range 1 to 100
-    return 100;
+    int randomNum = rand() % 101;
+    return randomNum;
 }
 
 
@@ -26,7 +27,10 @@ int generateRandomNumber() {
 ***/
 int getPlayerGuess() {
     // TODO: Ask the player guest and return the player's number
-
+    int guess;
+    cout << "Enter your guess (1-100):";
+    cin >> guess; 
+    cout << endl;
     return 1;
 }
 
@@ -45,8 +49,16 @@ string getAnswer(int number, int randomNumber) {
               If number is lower than randomNumber, the answer is "Your number is lower."
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
-    string answer;
+   string answer;
 
+   if (number > randomNumber) {
+    answer = "Your number is higher.";
+    } else if (number < randomNumber) {
+    answer = "Your number is lower.";
+    } else {
+    answer = "Congratulation! You win.";
+    }
+ 
     return answer;
 }
 
@@ -59,8 +71,10 @@ string getAnswer(int number, int randomNumber) {
 ***/
 bool checkSuccess(string answer) {
     // TODO: return the result after checking that player guessed right or wrong
-    
-    return true;
+    if (answer =="Your number is higher."||answer =="Your number is lower."){
+        return false;
+    }
+    else return true;
 }
 
 
@@ -73,7 +87,9 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-
+    if (isContinued =='y'){
+        result= true;
+    }
     return result;
 }
 
@@ -87,7 +103,9 @@ bool checkContinuePlaying(char isContinued) {
 char getPlayerOpinion() {
     // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
-
+    cout << "do you want to continue (y/n):";
+    cin >> isContinued;
+    cout << endl;
     return isContinued;
 }
 
